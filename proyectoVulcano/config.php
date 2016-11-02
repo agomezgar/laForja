0685ef10018eb6434ef6718355bb75f1f56d4c8b
agomezgar <agomezgar@gmail.com>
2016-11-02 12:19:01 +0100

    Carpetas redundantes borradas

--- proyectoVulcano/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servidor = 'localhost';
-$base = 'cunyado2';
-$usuario = 'root';
-$contra = 'antonio';
-$nombrecentro = 'IES Eduardo Valencia';$direccion = 'C/ Cervantes, 132, Calzada de Calatrava (Ciudad Real)';
-$curso = '2016/2017';
-?>
