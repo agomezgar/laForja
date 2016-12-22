fb35f5a6d7b26df32f26f15bab17b3dbb5558d0c
agomezgar <agomezgar@gmail.com>
2016-12-22 10:00:22 +0100

    Añadida evaluación por competencias en resumen  de trimestre

--- dameNotasGrupoPdf.php
+++ dameNotasGrupoPdf.php
@@ -19,6 +19,7 @@ $pesoPAvanzado=0;
 $nombreTablaInstrumentos=$materia."Instrumentos".$curso;
 $nombreTablaExamenes=$materia."notas".$curso;
 $nombreTablaEstandares=$materia."organizacionestandares".$curso;
+$nombreTablaCompetencias="competencias".$materia;
 $listaEstandares="estandares".$materia;
 $nombMateria=mysqli_query($link,"SELECT materia FROM materias WHERE codigo='$materia' and curso='$curso'");
 while ($nombremat=mysqli_fetch_array($nombMateria)){
@@ -63,17 +64,32 @@ $multIntermedio=$pesoPIntermedio/$nestandarI;
 $multAvanzado=$pesoPAvanzado/$nestandarA;
 
 $pdf->AddPage();
-$pdf->SetFont('Arial','B',16);
-$pdf->MultiCell(0,10,"Grupo: ".$grupo."\nNotas para la materia: ".$nombreMateria);
+$pdf->SetFont('Arial','B',14);
+$pdf->MultiCell(0,10,"Grupo: ".$grupo."\nNotas para la materia: ".$nombreMateria."\nTrimestre: ".$trimestre);
 $pdf->SetFont('Arial','B',12);
-$pdf->Cell(120,10,"Alumno",1);
-$pdf->Cell(55,10,"Nota del trimestre ".$trimestre,1);
+$pdf->Cell(50,10,"Alumno",1);
+$pdf->Cell(20,10,"Nota final",1);
+$pdf->Cell(15,10,"CM",1);
+$pdf->Cell(15,10,"CL",1);
+$pdf->Cell(15,10,"CD",1);
+$pdf->Cell(15,10,"SI",1);
+$pdf->Cell(15,10,"CEC",1);
+$pdf->Cell(15,10,"CSC",1);
+$pdf->Cell(15,10,"AA",1);
 $pdf->Ln();
 $pdf->SetFont('Arial','',10);
 $cadena="Para este trimestre hay asignados ".$nestandarB." estándares básicos (peso ".$pesoPBasico.", les corresponde a cada uno un multiplicador de ".number_format($multBasico,4)."), ".$nestandarI." estándares intermedios (peso ".$pesoPIntermedio.", les corresponde a cada uno un multiplicador de ".number_format($multIntermedio,4)."),  y ".$nestandarA." avanzados (peso ".$pesoPAvanzado.", les corresponde a cada uno un multiplicador de ".number_format($multAvanzado,4).")";
 
 $alumno=mysqli_query($link,"SELECT * FROM matriculas WHERE grupo='$grupo' ORDER BY apellidos")or die (mysqli_error($link));
 while($encuentraAlumno=mysqli_fetch_array($alumno)){
+//variables para medir competencias
+$CM=array();
+$CL=array();
+$CD=array();
+$SI=array();
+$CEC=array();
+$CSC=array();
+$AA=array();
 $ponAlumno=false;
 $idAlumno=$encuentraAlumno['alumno'];
 $buscaInstrumentos=mysqli_query($link,"SELECT * FROM $nombreTablaInstrumentos WHERE trimestre='$trimestre'");
@@ -107,6 +123,7 @@ $cuenta=0;
 $notaFinal=0;
 $alturaInicial=20;
 $textoNotas='';
+$notaInstrumento=0;
 if (mysqli_num_rows($buscaNotas)>1){
 while($encuentraNotas=mysqli_fetch_array($buscaNotas)){
 $nota=$nota+$encuentraNotas['nota'];
@@ -114,6 +131,7 @@ $cuenta++;
 }
 //echo "Instrumento: ".utf8_encode($nombre)." (Varias notas) Media: ".$nota/$cuenta."<br>";
 $textoNotas=$textoNotas.'Instrumento: '.utf8_encode($nombre).' (varias notas) Media: '.number_format($nota/$cuenta,2)."\n";
+$notaInstrumento=$nota/$cuenta;
 //$pdf->Cell(0,$altura,'Instrumento: '.$nombre.' (varias notas) Media: '.$nota/$cuenta,1);
 $altura+=5;
 }
@@ -125,9 +143,11 @@ $nota=$encuentraNotas['nota'];
 //echo "Instrumento: ".utf8_encode($nombre)." Nota: ".$nota."<br>";
 $textoNotas=$textoNotas.'Instrumento: '.utf8_encode($nombre).' Nota: '.number_format($nota,2)."\n";
 //$pdf->Cell(0,$altura,'Instrumento: '.$nombre.' Nota: '.$nota/$cuenta,1);
+$notaInstrumento=$nota;
 $altura+=5;
 }
 //$pdf->MultiCell(0,5,utf8_decode($textoNotas),1);
+
 }
 
 //BUSCAMOS ESTANDARES Y NOTAS CORRESPONDIENTES
@@ -144,6 +164,7 @@ $cadenaEstandar='';
 //echo "<fieldset>".utf8_encode($dameEst[0])."<br>";
 $cadenaEstandar=$cadenaEstandar.utf8_encode($dameEst[0])."\n";
 $listaInst=mysqli_query($link,"SELECT DISTINCT * FROM $nombreTablaEstandares WHERE idestandar=".$buscaSql['idestandar']." AND trimestre=".$trimestre) or die(mysqli_error($link));
+$estandarActual=$buscaSql['idestandar'];
 while($encEst=mysqli_fetch_array($listaInst)){
 $buscInst=mysqli_query($link,"SELECT * FROM $nombreTablaInstrumentos WHERE id=".$encEst['idinstrumento']);
 while($encInst=mysqli_fetch_array($buscInst)){
@@ -173,7 +194,33 @@ $notaInstrumento= $encuentraEx['nota'];
 $cadenaEstandar=$cadenaEstandar."Nota: ".number_format($notaInstrumento,2)."\n";
 $notaEstandar=$notaEstandar+$notaInstrumento;
 }
-
+//ASIGNAMOS LA NOTA DE LOS INSTRUMENTOS UTILIZADOS TAMBIÉN A LAS COMPETENCIAS PERTINENTES PARA ESE ESTÁNDAR
+$buscaComp=mysqli_query($link,"SELECT competencia FROM $nombreTablaCompetencias WHERE contenido=$estandarActual");
+while($encuentraComp=mysqli_fetch_array($buscaComp)){
+switch ($encuentraComp['competencia']){
+case 1:
+$CM[]=$notaInstrumento;
+break;
+case 2:
+$CL[]=$notaInstrumento;
+break;
+case 3:
+$CD[]=$notaInstrumento;
+break;
+case 4:
+$SI[]=$notaInstrumento;
+break;
+case 5:
+$CEC[]=$notaInstrumento;
+break;
+case 6:
+$CSC[]=$notaInstrumento;
+break;
+case 7:
+$AA[]=$notaInstrumento;
+break;
+}
+}
 }
 
 }
@@ -210,12 +257,85 @@ $cadenaEstandar=$cadenaEstandar."\nPrioridad de estandar: Avanzado"."\nAportaci�
 
 }
 $pdf->SetFont('Arial','',10);
-$pdf->Cell(120,10,$nombreAlumno,1);
-$pdf->Cell(55,10,number_format($notaAcumulada,2),1);
+$pdf->Cell(50,10,$nombreAlumno,1);
+$pdf->Cell(20,10,number_format($notaAcumulada,2),1);
+//Calculamos la nota en cada competencia
+if(empty($CM)){
+$pdf->Cell(15,10,"No",1);
+}else{
+$notaCompet=0;
+for ($i=0;$i<count($CM);$i++){
+$notaCompet=$notaCompet+(float)$CM[$i];
+}
+$pdf->Cell(15,10,number_format(($notaCompet/count($CM)),2),1);
+}
+if(empty($CL)){
+$pdf->Cell(15,10,"No",1);
+}else{
+$notaCompet=0;
+for ($i=0;$i<count($CL);$i++){
+$notaCompet=$notaCompet+(float)$CL[$i];
+}
+$pdf->Cell(15,10,number_format(($notaCompet/count($CL)),2),1);
+}
+if(empty($CD)){
+$pdf->Cell(15,10,"No",1);
+}else{
+$notaCompet=0;
+for ($i=0;$i<count($CD);$i++){
+$notaCompet=$notaCompet+(float)$CD[$i];
+}
+$pdf->Cell(15,10,number_format(($notaCompet/count($CD)),2),1);
+}
+if(empty($SI)){
+$pdf->Cell(15,10,"No",1);
+}else{
+$notaCompet=0;
+for ($i=0;$i<count($SI);$i++){
+$notaCompet=$notaCompet+(float)$SI[$i];
+}
+$pdf->Cell(15,10,number_format(($notaCompet/count($SI)),2),1);
+}
+if(empty($CEC)){
+$pdf->Cell(15,10,"No",1);
+}else{
+$notaCompet=0;
+for ($i=0;$i<count($CEC);$i++){
+$notaCompet=$notaCompet+(float)$CEC[$i];
+}
+$pdf->Cell(15,10,number_format(($notaCompet/count($CEC)),2),1);
+}
+if(empty($CSC)){
+$pdf->Cell(15,10,"No",1);
+}else{
+$notaCompet=0;
+for ($i=0;$i<count($CSC);$i++){
+$notaCompet=$notaCompet+(float)$CSC[$i];
+}
+$pdf->Cell(15,10,number_format(($notaCompet/count($CSC)),2),1);
+}
+if(empty($AA)){
+$pdf->Cell(15,10,"No",1);
+}else{
+$notaCompet=0;
+for ($i=0;$i<count($AA);$i++){
+$notaCompet=$notaCompet+(float)$AA[$i];
+}
+$pdf->Cell(15,10,number_format(($notaCompet/count($AA)),2),1);
+}
+//$pdf->Cell(15,10,"CM",1);
+
 $pdf->Ln();
 }
 
 }
+$pdf->Ln();
+$pdf->SetFont('Arial','',8);
+$cadena="Competencias: CM: Competencia Matemática, CL: Competencia Lingüística, CD: Competencia Digital, SI: Sentido de la Iniciativa y Espíritu ";
+$pdf->Cell(0,5,utf8_decode($cadena));
+$cadena="Emprendedor, CEC: Conciencia y Espresiones Culturales, CSC: Competencias Sociales y Cívicas, AA: Aprender a aprender";
+$pdf->Ln();
+$pdf->Cell(0,5,utf8_decode($cadena));
 $pdf->Output();
 
 
